4851f3e92f2c90521fbdd962f39c0cb54761c137
Konstantin <zkvprog@gmail.com>
2023-06-01 12:02:52 +0300

    Init DI container

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,23 @@
 <?php
 
+use App\Db\PdoConnector;
+use App\System\DIContainer;
+use App\System\DIContainerFacade;
+
 require 'vendor/autoload.php';
 
 session_start();
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
+$diContainer = new DIContainer([
+    'db' => function () {
+        return PdoConnector::getInstance();
+    },
+    'webauthn' => function () {
+        return new \lbuchs\WebAuthn\WebAuthn($_ENV['APP_NAME'], $_ENV['DOMAIN_NAME']);
+    }
+]);
+
+DIContainerFacade::setContainer($diContainer);
